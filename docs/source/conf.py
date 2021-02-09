adb8a4c26ade0b7c3188b5c07213f44f67e73ff6
Max O'Brien <max@maxobrien.co.uk>
2021-02-09 12:40:19 +0000

    Updated theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,21 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
+
+html_title = "SUFST Documentation"
+html_logo = "img/stag.svg"
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#0066cc",
+        "color-brand-content": "#0066cc",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#0066cc",
+        "color-brand-content": "#0066cc",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
